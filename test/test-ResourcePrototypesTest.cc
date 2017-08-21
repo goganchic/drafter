ff4c151beb3013023b63bad6d699d53c14bf8051
e.baranov <e.baranov@fun-box.ru>
2017-08-21 16:52:41 +0300

    add more tests

--- test/test-ResourcePrototypesTest.cc
+++ test/test-ResourcePrototypesTest.cc
@@ -3,6 +3,10 @@
 
 using namespace draftertest;
 
-TEST_REFRACT("resource-prototypes", "simple");
-TEST_REFRACT("resource-prototypes", "nested");
+TEST_REFRACT("resource-prototypes", "resource-group");
 TEST_REFRACT("resource-prototypes", "multiple-groups");
+TEST_REFRACT("resource-prototypes", "resource");
+TEST_REFRACT("resource-prototypes", "named-resource");
+TEST_REFRACT("resource-prototypes", "resource-named-endpoint");
+TEST_REFRACT("resource-prototypes", "group-and-resource");
+TEST_REFRACT("resource-prototypes", "prototype-inheritance");
